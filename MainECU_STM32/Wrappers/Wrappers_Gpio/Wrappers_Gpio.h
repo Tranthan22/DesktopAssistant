7818cb9d6186893395cc6ead4e77a0864e30d00e
Tran Than <tranbathan.02@gmail.com>
2025-02-15 21:56:57 +0700

    Dev_Wrappers_Gpio Add Read,Write pin functions

--- MainECU_STM32/Wrappers/Wrappers_Gpio/Wrappers_Gpio.h
+++ MainECU_STM32/Wrappers/Wrappers_Gpio/Wrappers_Gpio.h
@@ -1,6 +1,9 @@
 #ifndef _WRAPPERS_GPIO_H_
 #define _WRAPPERS_GPIO_H_
 
+#define Wrappers_Gpio_Write(p_LogicalChannel_u8, p_LogicLevel_u8)       HAL_GPIO_WritePin(p_LogicalChannel_u8, p_LogicLevel_u8)
+#define Wrappers_Gpio_Read(p_LogicalChannel_u8)                         HAL_GPIO_ReadPin(p_LogicalChannel_u8)
+
 void Wrappers_Gpio_Init(void);
 
 #endif /* _WRAPPERS_GPIO_H_ */
